3a96fff64390ed4c0b04e2f84413482a70eeaccb
satya <satyapraveen578@gmail.com>
2023-12-27 20:54:38 +0530

    my task completed

--- mysatyaRegPage.java
+++ mysatyaRegPage.java
@@ -1,2 +1,7 @@
 this is satya Reg
 this is my manager work completed
+1st day of java lines of code
+2nd day of java lines of code
+3rd day of java lines of code
+4th day of java lines of code
+5th day of java lines of code
